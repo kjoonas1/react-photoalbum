d041f9c206abc0d16495a09e4ea12ef6d2642702
kjoonas1 <joonas.k@hotmail.com>
2019-12-06 13:15:09 +0200

    First boilerplate + configs

---
+++ photoalbum/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import PhotoGrid from "./components/PhotoGrid"
+
+const App = () => {
+  return (
+    <PhotoGrid />
+  )
+}
+
+export default App
---
+++ photoalbum/src/components/PhotoGrid.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default () => {
+
+    return (
+        <div className="grid-container">
+            <div className="grid-item">
+                Hello
+            </div>
+        </div>
+    )
+}
